e739849a857d06759ec49fa49c7af5118dc0e5db
Marlon Andrei <marlon-master@hotmail.com>
2022-04-18 21:47:34 -0300

    firt commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// PAGES
+import { HomeComponent } from './pages/home/home.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'sobre', component: SobreComponent },
+  { path: '404', component: PageErrorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
